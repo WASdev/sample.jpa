0d49f645be2c5449b9260e46eb407dd853f145bd
BillyD73 <Billy.Dawson3@uk.ibm.com>
2017-02-01 15:36:48 +0000

    Fixed URL

--- src/test/java/it/EndpointTest.java
+++ src/test/java/it/EndpointTest.java
@@ -27,7 +27,9 @@ public class EndpointTest {
 
     public void testEndpoint(String endpoint, String expectedOutput) {
         String port = System.getProperty("liberty.test.port");
-        String war = System.getProperty("war.name");
+        String war = System.getProperty("war.context");
+    	String apploc = System.getProperty("appLocation");
+    	System.out.println("Port: " + port + "\nWar: " + war + "\nApploc: " + apploc);
         String url = "http://localhost:" + port + "/" + war + endpoint;
         System.out.println("Testing " + url);
         Response response = sendRequest(url, "GET");
